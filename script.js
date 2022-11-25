ca13fa934c0c4d8966e8e9aa34b627739e469339
Julian Betts <wjulianbetts@gmail.com>
2022-11-24 17:20:56 -0800

    js update

--- script.js
+++ script.js
@@ -1,28 +1,44 @@
 var currentHour = moment().hour();
-//console.log(currentHour)
+var displayDate = moment().format("dddd, MMMM Do YYYY, h:mm:ss a")
+$("#currentDay").text(displayDate)
+
+function saveText(event) {
+    var text = $(event.target).sibling(".description").val()
+    var textTime = $(event.target).siblings(".time").text()
+    console.log(text)
+    localStorage.setItem(txtTime, text);
+}
+
 for (let i = 9; i < 18; i++){
     var tense = "past";
     var hour = moment().hour(i).hour();
-    console.log(hour);
+    console.log(hour)
+
     if (currentHour == hour) {
         tense = "present";
-    }   else if (hour > currentHour){
-        tense = "future";
-    } 
+    }    else if (hour > currerntHour){
+            tense = "future"
+        }
+    }
     console.log(tense)
-       
+
+    
+    
     var hourElement = document.createElement("tr");
     $(hourElement).addClass(tense);
-    var time = $(document.createElement("td")).addClass("time"); //$ calls jquery//
-    var description = $(document.createElement("td")).addClass("description").addClass("event");
-    $(time).append(moment().hour(i).format("ha"));
+    var time = $("<tb>").addClass("time");
+    $(time).text(moment().hour(i).format("ha"));
+    var storage = localStorage.getitem(time.text())
+    var description = $("<input>").addClass("description").addClass("event").val(storage);
+    var btn = $("<button>").text("add event").on("click", saveText);
     $(hourElement).append(time);
     $(hourElement).append(description);
+    $(hourElement).append(btn);
     $("#calendar").append(hourElement);
-    //console.log(moment().hourElement(i).hourElement());
-    //console.log(moment().hourElement(i).format("ha"));
-}
 
 
-//edit events
+
+
+
+
 
